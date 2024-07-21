f0a057c3e351471d2a3dafc097259ad9d7972ab6
Abenezer <aben.ezer443@gmail.com>
2024-07-21 09:36:48 +0300

    Adding updates

--- sage/src/app/details/page.tsx
+++ sage/src/app/details/page.tsx
@@ -4,30 +4,38 @@ import React from "react";
 function Details() {
   return (
     <div className="grid grid-cols-3 gap-2 h-screen bg-[url('/background.jpg')] bg-cover p-20">
-      <div className="gird grid-cols-2 gap-2">
-        <img
-          src="/img1.jpg"
-          alt=""
-          className="h-32 border object-fit rounded-lg"
-        />
-        <img
-          src="/img1.jpg"
-          alt=""
-          className="h-32 border object-fit rounded-lg"
-        />
-        <img
-          src="/img1.jpg"
-          alt=""
-          className="h-32 border object-fit rounded-lg"
-        />
-        <img
-          src="/img1.jpg"
-          alt=""
-          className="h-32 border object-fit rounded-lg"
-        />
+      <div className="flex flex-wrap gap-8">
+        <div>
+          <img
+            src="/img1.jpg"
+            alt=""
+            className="w-40 h-40 border rounded-lg bg-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="/img1.jpg"
+            alt=""
+            className="w-40 h-40 border rounded-lg bg-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="/img1.jpg"
+            alt=""
+            className="w-40 h-40 border rounded-lg bg-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="/img1.jpg"
+            alt=""
+            className="w-40 h-40 border rounded-lg bg-cover"
+          />
+        </div>
       </div>
-      <div className="bg-[rgba(113, 113, 113, 0.4)] h-auto">
-        <p>
+      <div className="bg-[rgba(113, 113, 113, 0.4)] h-auto bg-grey-200">
+        <p className="text-3xl">
           Seuiuhjksdlskjhdflsfsdhjfh skjdhksfh dkjfhjsk djfhsdkjf hskdfjhs
           kdjfhsd kjfhskd hsdfj hskdf skdjfh skjdfh skhks hfs jfhsjh fsdhfksjh
           dfklsjhdf lskjdh flskjhf lsjh flsjdhf skljdh sdjkhfskdjh skldjfh skdjh
