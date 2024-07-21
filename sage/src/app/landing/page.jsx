be8df0888e9e323cd0fa26ee3499a7eed07d8183
Tadiwos Anegagregn <tadiwosanegagregn@gmail.com>
2024-07-21 15:11:34 +0300

    adding landing page content not changed

---
+++ sage/src/app/landing/page.jsx
@@ -0,0 +1,31 @@
+
+import Service from '../../components/Service'
+ import Header from '../../components/Header'
+ import Hero from '../../components/Hero'
+import ThirdSec from '../../components/ThirdSec'
+import How from '../../components/How'
+import Bottom from '../../components/Bottom'
+import { AccordionDemo } from '../../components/Demo/AccordionDemo'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Header/>
+     <Hero/>   
+    <Service/>  
+    <ThirdSec/>
+    <How/>
+    
+   <AccordionDemo/>
+
+    
+    <Bottom/> 
+    
+    </>
+  )
+}
+
+export default App
