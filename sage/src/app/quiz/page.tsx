546bce02d23e8696ab9f6f435d76307dd90f4f0c
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-07-21 06:00:37 +0300

    fixed progressbar isuues fixed image auto adjsut fixed

--- sage/src/app/quiz/page.tsx
+++ sage/src/app/quiz/page.tsx
@@ -5,6 +5,7 @@ const quizData = {
   quiz: [
     {
       question: "What is the sum of 2+2?",
+      imageurl: "https://images.unsplash.com/photo-1626820000000-4b3b3b3b3b3b",
       answers: {
         a: "1",
         b: "2",
@@ -31,7 +32,7 @@ const quizData = {
         c: "4",
         d: "6",
       },
-      correct: "a",
+      correct: "c",
     },
     {
       question: "What is the quotient of 12/3?",
@@ -41,14 +42,14 @@ const quizData = {
         c: "4",
         d: "6",
       },
-      correct: "a",
+      correct: "c",
     },
     {
       question: "What is the value of x in the equation 2x = 6?",
       answers: {
         a: "1",
-        b: "2",
-        c: "3",
+        b: "3",
+        c: "2",
         d: "4",
       },
       correct: "b",
@@ -76,9 +77,9 @@ const quizData = {
     {
       question: "What is the result of 8-2?",
       answers: {
-        a: "4",
+        a: "6",
         b: "5",
-        c: "6",
+        c: "4",
         d: "7",
       },
       correct: "a",
@@ -86,9 +87,9 @@ const quizData = {
     {
       question: "What is the quotient of 24/4?",
       answers: {
-        a: "4",
+        a: "6",
         b: "5",
-        c: "6",
+        c: "9",
         d: "8",
       },
       correct: "a",
@@ -114,9 +115,12 @@ const CapitalQuiz = () => {
   const [time, setTime] = useState(0);
 
   const handleChoiceSelect = (choice) => {
+    // console.log(choice);
     setSelectedChoice(choice);
   };
-  const handleCheckClick = () => {
+
+  const handleCheckClick = (e) => {
+    e.preventDefault();
     const correctAnswer = quizData.quiz[currentQuestionIndex].correct;
     if (selectedChoice === correctAnswer) {
       setScore(score + 1);
@@ -126,23 +130,27 @@ const CapitalQuiz = () => {
       setSelectedChoice(null);
       setTime(0);
     } else {
+      setProgress(currentQuestionIndex + 1);
       alert(
-        `Quiz finished! Your score is ${
-          score + (selectedChoice === correctAnswer ? 1 : 0)
+        `Quiz finished! Your score is ${score + (selectedChoice === correctAnswer ? 1 : 0)
         }/${quizData.quiz.length}`
       );
+
     }
   };
+
   useEffect(() => {
     const timer = setInterval(() => {
       setTime((prevTime) => prevTime + 1);
     }, 1000);
     return () => clearInterval(timer);
-  }, [currentQuestionIndex]);
+  }, [time]);
+
 
   useEffect(() => {
-    setProgress((currentQuestionIndex / quizData.quiz.length) * 100);
+    setProgress(currentQuestionIndex);
   }, [currentQuestionIndex]);
+
   const currentQuestion = quizData.quiz[currentQuestionIndex];
 
   return (
@@ -151,129 +159,33 @@ const CapitalQuiz = () => {
         <ProgressBar
           value={progress}
           max={quizData.quiz.length}
-          // className="w-full mb-4"
+        // className="w-full mb-4"
         />
+        <div className="mt-4 text-xl">Score: {score}</div>
+        <div className="mt-2 text-md">Time: {time} seconds</div>
         <ChoiceCardFullPicture
+          imageurl={currentQuestion.imageurl}
           question={currentQuestion.question}
           answers={currentQuestion.answers}
           selectedChoice={selectedChoice}
           onChoiceSelect={handleChoiceSelect}
         />
-        {/* <div className="text-2xl font-bold text-gray-800 mb-4">
-          What is the capital of France?
-        </div>
-        <div className="grid grid-cols-4 gap-3">
-          <div
-            className={`flex flex-col items-center cursor-pointer rounded-md p-4 transition-colors ${
-              selectedChoice === "Paris"
-                ? "bg-blue-200"
-                : "bg-gray-100 hover:bg-gray-200"
-            }`}
-            onClick={() => handleChoiceClick("Paris")}
-          >
-            <img
-              src="https://via.placeholder.com/300x200"
-              alt="Choice 1"
-              className="w-full h-48 rounded-md object-cover mb-3"
-            />
-            <div className="flex flex-row  justify-evenly ">
-              <input
-                type="radio"
-                name="choice"
-                checked={selectedChoice === "Paris"}
-                className="mr-2"
-              />
-              <span className="text-gray-800 text-xl font-bold">Paris</span>
-            </div>
-          </div>
-          <div
-            className={`flex flex-col items-center cursor-pointer rounded-md p-4 transition-colors ${
-              selectedChoice === "London"
-                ? "bg-blue-200"
-                : "bg-gray-100 hover:bg-gray-200"
-            }`}
-            onClick={() => handleChoiceClick("London")}
-          >
-            {" "}
-            <img
-              src="https://via.placeholder.com/300x200"
-              alt="Choice 2"
-              className="w-full h-48 rounded-md object-cover mb-3"
-            />
-            <div className="flex flex-row  justify-evenly ">
-              <input
-                type="radio"
-                name="choice"
-                checked={selectedChoice === "London"}
-                className="mr-2"
-              />
-              <span className="text-gray-800 text-xl font-bold">London</span>
-            </div>
-          </div>
-          <div
-            className={`flex flex-col items-center cursor-pointer rounded-md p-4 transition-colors ${
-              selectedChoice === "Berlin"
-                ? "bg-blue-200"
-                : "bg-gray-100 hover:bg-gray-200"
-            }`}
-            onClick={() => handleChoiceClick("Berlin")}
-          >
-            <img
-              src="https://via.placeholder.com/300x200"
-              alt="Choice 3"
-              className="w-full h-48 rounded-md object-cover mb-3"
-            />
-            <div className="flex flex-row  justify-evenly ">
-              <input
-                type="radio"
-                name="choice"
-                checked={selectedChoice === "Berlin"}
-                className="mr-2"
-              />
-              <span className="text-gray-800 text-xl font-bold">Berlin</span>
-            </div>
-          </div>
-          <div
-            className={`flex flex-col items-center cursor-pointer rounded-md p-4 transition-colors ${
-              selectedChoice === "Rome"
-                ? "bg-blue-200"
-                : "bg-gray-100 hover:bg-gray-200"
-            }`}
-            onClick={() => handleChoiceClick("Rome")}
-          >
-            <img
-              src="https://via.placeholder.com/300x200"
-              alt="Choice 4"
-              className="w-full h-48 rounded-md object-cover mb-3"
-            />
-            <div className="flex flex-row  justify-evenly ">
-              <input
-                type="radio"
-                name="choice"
-                checked={selectedChoice === "Rome"}
-                className="mr-2"
-              />
-              <span className="text-gray-800 text-xl font-bold">Rome</span>
-            </div>
-          </div>
-        </div> */}
         <button
           className="block w-full bg-green-500 hover:bg-green-600 text-white rounded-md py-3 px-4 mt-6 cursor-pointer transition-colors"
           onClick={handleCheckClick}
         >
           Check
         </button>
-        <div className="mt-4 text-xl">Score: {score}</div>
-        // <div className="mt-2 text-md">Time: {time} seconds</div>
+
       </div>
     </div>
   );
 };
 
 export default CapitalQuiz;
-const ProgressBar = ({ value, max }) => {
+const ProgressBar = ({ value, max }: { value: number, max: number }) => {
   const percentage = (value / max) * 100;
-
+  console.log(percentage, "%");
   return (
     <div className="w-full bg-gray-200 rounded-full h-6">
       <div
